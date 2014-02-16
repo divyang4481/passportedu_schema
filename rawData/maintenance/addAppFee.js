f50a6d0cef39d19c8ad4e2572b870cfc0c698764
Brian Johnson <bjohnson@trulia.com>
2014-02-15 19:19:34 -0800

    Fixing a couple missing "s"s

--- rawData/maintenance/addAppFee.js
+++ rawData/maintenance/addAppFee.js
@@ -1,8 +1,18 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , fs = require('fs')
+  , mongoose = require('mongoose')
+  , io = require('socket.io');
+// Mongoose
+mongoose.connect('mongodb://localhost/psprt');
 var application = require('../../server/models/application');
 application.find({}, function(er, apps) {
   for(var u = 0; u < apps.length; u++) {
     apps[u].fee = 30.00;
-    apps[u].save(function(err) {
+    apps[u].markModified('fee');
+    apps[u].save(function(err, App) {
+      console.log(err, App);
     });
   }
 });
--- rawData/maintenance/addFeeArray.js
+++ rawData/maintenance/addFeeArray.js
@@ -1,7 +1,16 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , fs = require('fs')
+  , mongoose = require('mongoose')
+  , io = require('socket.io');
+// Mongoose
+mongoose.connect('mongodb://localhost/psprt');
 var user = require('../../server/models/user');
 user.find({}, function(er, users) {
   for(var u = 0; u < users.length; u++) {
     users[u].feesPaid = [];
+    users[u].markModified('feesPaid');
     users[u].save(function(err) {
     });
   }
